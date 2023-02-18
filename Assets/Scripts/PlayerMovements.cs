e58fa2038fa780e4bd29c7d154695967eef80413
SungHoon <hooni.dev@gmail.com>
2023-02-18 17:34:48 +0900

    Camera X Rot By Cinemachine

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -9,13 +9,21 @@ public class PlayerMovements : MonoBehaviour
 
     private float moveSpeed = 5f;
     private float jumpPower = 5f;
-    private Vector3 input;
+    private float mouseSens = 3f;
+
+    private Vector3 moveInput;
+    private Vector2 mouseInput;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
 
     private void Update()
     {
-        input = Vector3.zero;
-        input.x = Input.GetAxisRaw("Horizontal");
-        input.z = Input.GetAxisRaw("Vertical");
+        moveInput = new(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        mouseInput = new(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -25,9 +33,11 @@ public class PlayerMovements : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 moveDir = transform.TransformDirection(input * moveSpeed);
+        Vector3 moveDir = transform.TransformDirection(moveInput) * moveSpeed;
 
         moveDir = Vector3.Lerp(rb.velocity, moveDir, Time.deltaTime * 5f);
         rb.velocity = new Vector3(moveDir.x, rb.velocity.y, moveDir.z);
+
+        transform.Rotate(new Vector3(0f, mouseInput.x * mouseSens, 0f));
     }
 }
